771da7940f86394560634b8a801803de2b8e6a8e
gek135 <Hyzazavum1>
2024-06-03 14:50:22 +0300

    Генерация данных только на неделю
    (cherry picked from commit fbb7416e436667a4ed0dbce26043161f3aedb5a6)

--- src/Controller/ScheduleController.php
+++ src/Controller/ScheduleController.php
@@ -13,13 +13,14 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ScheduleController extends AbstractController implements NavElementInterface {
 
-    public function __construct(private AlgorithmService $service) {}
+    public function __construct(private AlgorithmService $service, private DataService $service2) {}
     #[Route('/schedule', name: 'app_schedule')]
     public function index(): Response {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         $user = $this->getUser();
-        $this->service->run();
+        $this->service2->generateInputData();
+        //$this->service->run();
         dd(1);
         return $this->render('schedule/index.html.twig', [
             'controller_name' => 'ScheduleController',
--- src/Modules/Algorithm/DataService.php
+++ src/Modules/Algorithm/DataService.php
@@ -50,8 +50,9 @@ class DataService
 
     public function generateInputData(): void
     {
+        set_time_limit(600);
         //TODO: Перенести в БД входные данные
-        $start_date = new \DateTime("2021-01-01");
+        $start_date = new \DateTime("2024-05-17");
         $end_date = new \DateTime("2024-05-24");
         $interval = new \DateInterval('P1W');
         $period = new \DatePeriod($start_date, $interval, $end_date);
@@ -60,13 +61,13 @@ class DataService
         $modalities = ['X-ray', 'CT', 'MRI', 'US', 'PET'];
         $study_types = ['Ортопедические', 'Неврологические', 'Абдоминальные', 'Кардиологические', 'Грудные', 'Другие'];
 
-        $studies = [];
+        $studiesByWeek = [];
         foreach ($period as $date) {
             foreach ($modalities as $modality) {
                 foreach ($study_types as $study_type) {
                     $num_studies = rand(0, 100);
-                    $studies[] = [
-                        'Дата' => $date->format('Y-m-d'),
+                    $studiesByWeek[] = [
+                        'Дата' => $date,
                         'Модальность' => $modality,
                         'Вид исследования' => $study_type,
                         'Количество исследований' => $num_studies
@@ -75,8 +76,20 @@ class DataService
             }
         }
 
+        $studiesByDay = [];
+
+        foreach ($studiesByWeek as $studies) {
+            for ($i=1; $i <= $studies['Количество исследований']; $i++) {
+                $studiesByDay[] = [
+                    'Дата' => $this->getRandomDateInPeriod($studies['Дата'], $studies['Дата']->modify('+7 days')),
+                    'Модальность' => $studies['Модальность'],
+                    'Вид исследования' => $studies['Вид исследования'],
+                ];
+            }
+        }
+
         // Нормы количества описанных исследований на одного врача в смену
-        $norms = [
+        /*$norms = [
             ['Модальность' => 'X-ray', 'Вид исследования' => 'Ортопедические', 'Норма в смену' => 40],
             ['Модальность' => 'X-ray', 'Вид исследования' => 'Неврологические', 'Норма в смену' => 30],
             ['Модальность' => 'CT', 'Вид исследования' => 'Абдоминальные', 'Норма в смену' => 20],
@@ -84,23 +97,32 @@ class DataService
             ['Модальность' => 'US', 'Вид исследования' => 'Грудные', 'Норма в смену' => 25],
             ['Модальность' => 'PET', 'Вид исследования' => 'Другие', 'Норма в смену' => 10],
             // Добавьте остальные комбинации
-        ];
+        ];*/
 
         // Список врачей с описанием их компетенций
-        $doctors = [
+        /*$doctors = [
             ['ID' => 'Врач 1', 'Компетенции' => ['Модальности' => ['X-ray', 'CT'], 'Виды исследований' => ['Ортопедические', 'Абдоминальные']]],
             ['ID' => 'Врач 2', 'Компетенции' => ['Модальности' => ['MRI', 'US'], 'Виды исследований' => ['Неврологические', 'Кардиологические']]],
             ['ID' => 'Врач 3', 'Компетенции' => ['Модальности' => ['PET'], 'Виды исследований' => ['Грудные', 'Другие']]],
             // Добавьте остальных врачей
-        ];
+        ];*/
 
         // Сохранение данных
         file_put_contents(
             __DIR__.'/mocks/generatedData.json',
             [
-                'Количество исследований' => json_encode($studies, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
-                'Нормы' => json_encode($norms, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
-                'Врачи' => json_encode($doctors, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
+                'Количество исследований' => json_encode($studiesByDay, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
+                //'Нормы' => json_encode($norms, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
+                //'Врачи' => json_encode($doctors, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
             ]);
+    }
+
+    private function getRandomDateInPeriod(\DateTime $firstDate, \DateTime $secondDate): string
+    {
+        if ($firstDate < $secondDate) {
+            return date('Y-m-d', mt_rand($firstDate->getTimestamp(), $secondDate->getTimestamp()));
+        }
+
+        return date('Y-m-d', mt_rand($secondDate->getTimestamp(), $firstDate->getTimestamp()));
     }
 }
