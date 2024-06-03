1b51416963d5403cf9ef0ee82c4b62f19e1e2510
gek135 <Hyzazavum1>
2024-06-03 22:14:30 +0300

    Проверка возможности врача провести исследование с соблюдением норм
    (cherry picked from commit a5bfb967f98f8d6990de56c564c3449b6e4342b5)

---
+++ src/Entity/Studies.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class Studies
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(targetEntity: Competencies::class)]
+    private Competencies $competency;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private \DateTime|null $date = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEndTime(): ?\DateTime
+    {
+        return $this->date->modify('+ '.$this->getCompetency()->getDuration().' minutes');
+    }
+
+    public function getDate(): ?\DateTime
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTime $date): Studies
+    {
+        $this->date = $date;
+        return $this;
+    }
+
+    public function getCompetency(): Competencies
+    {
+        return $this->competency;
+    }
+
+    public function setCompetency(Competencies $competency): Studies
+    {
+        $this->competency = $competency;
+        return $this;
+    }
+
+    public function isNight(): bool
+    {
+        $startNight = (clone $this->date)->setTime(22, 0);
+
+        return $this->date >= $startNight;
+    }
+
+    public function isDay(): bool
+    {
+        $startNight = (clone $this->date)->setTime(22, 0);
+        $startDay = (clone $this->date)->setTime(6, 0);
+
+        return $this->date <= $startNight && $this->date >= $startDay;
+    }
+}
