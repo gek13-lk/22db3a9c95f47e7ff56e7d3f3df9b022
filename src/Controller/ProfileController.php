c09d426cf1b21f653e85da20ccd41f898cf0914d
Yuriy Yurinskiy <yuriyyurinskiy@yandex.ru>
2024-06-03 08:00:21 +0300

    build: добавляет doctrine, авотризацию, регистрацию
    (cherry picked from commit e32e9df69f08fbe3544973b9b4ce5705a24ef141)

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function index(): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $user = $this->getUser();
+
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
+            'username' => $user->getUserIdentifier(),
+        ]);
+    }
+}
