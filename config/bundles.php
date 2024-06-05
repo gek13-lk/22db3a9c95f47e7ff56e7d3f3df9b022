46c625848a0c1b095beed36d808f0db79220c8df
Yuriy Yurinskiy <yuriyyurinskiy@yandex.ru>
2024-06-05 23:48:07 +0700

    feat: роли
    (cherry picked from commit 5e79657856a8527569e138916efaffd253b8fd6d)

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
