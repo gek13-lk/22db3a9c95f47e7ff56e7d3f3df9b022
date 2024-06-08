acf0c82b44df78fc1b09f22f3a87170a84ef9d91
gek135 <Hyzazavum1>
2024-06-08 23:58:08 +0700

    Улучшение алгоритма

--- src/Entity/Doctor.php
+++ src/Entity/Doctor.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+use App\Repository\DoctorRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: DoctorRepository::class)]
 class Doctor
 {
     #[ORM\Id]
