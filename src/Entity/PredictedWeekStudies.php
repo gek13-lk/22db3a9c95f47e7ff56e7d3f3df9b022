59c4a873c710eca53da5eb55776f69af9b52245f
citrusonya <citrusonya@gmail.com>
2024-06-11 00:33:22 +0300

    Обучение моделей

---
+++ src/Entity/PredictedWeekStudies.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Table(name: 'week_studies_predicted')]
+#[ORM\Entity]
+class PredictedWeekStudies
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\ManyToOne(targetEntity: Competencies::class)]
+    private Competencies $competency;
+
+    #[ORM\Column(type: 'integer')]
+    private int $weekNumber;
+
+    #[ORM\Column(type: 'integer')]
+    private int $year;
+
+    #[ORM\Column(type: 'integer')]
+    private int $count;
+
+    #[ORM\Column(type: 'boolean', options: ['default' => true])]
+    private bool $isNew = true;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCompetency(): Competencies
+    {
+        return $this->competency;
+    }
+
+    public function setCompetency(Competencies $competency): PredictedWeekStudies
+    {
+        $this->competency = $competency;
+
+        return $this;
+    }
+
+    public function getWeekNumber(): int
+    {
+        return $this->weekNumber;
+    }
+
+    public function setWeekNumber(int $weekNumber): PredictedWeekStudies
+    {
+        $this->weekNumber = $weekNumber;
+
+        return $this;
+    }
+
+    public function getYear(): int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): PredictedWeekStudies
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getCount(): int
+    {
+        return $this->count;
+    }
+
+    public function setCount(int $count): PredictedWeekStudies
+    {
+        $this->count = $count;
+
+        return $this;
+    }
+
+    public function isNew(): bool
+    {
+        return $this->isNew;
+    }
+
+    public function setIsNew(bool $new = true): self
+    {
+        $this->isNew = $new;
+
+        return $this;
+    }
+
+    public function setIsNotNew(): self
+    {
+        $this->setIsNew(false);
+
+        return $this;
+    }
+}
