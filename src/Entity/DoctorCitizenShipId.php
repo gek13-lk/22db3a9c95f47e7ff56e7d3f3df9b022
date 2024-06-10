a6db208b5c3c298a508d1b08cae84741725b9526
Mikhail Pipich <m.pipich@bftcom.com>
2024-06-10 22:50:05 +0700

    Сущности и миграция по на таблицы с информацией о докторах

---
+++ src/Entity/DoctorCitizenShipId.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class DoctorCitizenShipId
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+    #[ORM\Column(type: 'integer', nullable: true, options: ["comment" => "Гражданство (id) Справочник ФРНСИ «Категории гражданства», OID 1.2.643.5.1.13.13.99.2.315"])]
+    private ?int $code = null;
+    #[ORM\Column(type: 'string', length: 255, nullable: true, options: ["comment" => "Наименование"])]
+    private ?string $name = null;
+    #[ORM\OneToOne(targetEntity: DoctorInfo::class, inversedBy: 'citizenShipId')]
+    private DoctorInfo $doctorInfo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
+
+    public function setCode(?int $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDoctorInfo(): ?DoctorInfo
+    {
+        return $this->doctorInfo;
+    }
+
+    public function setDoctorInfo(?DoctorInfo $doctorInfo): static
+    {
+        $this->doctorInfo = $doctorInfo;
+
+        return $this;
+    }
+}
