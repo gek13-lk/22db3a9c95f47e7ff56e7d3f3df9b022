7bdf5b8a3a23ebd1064076be3c94a25ad5715ae1
citrusonya <citrusonya@gmail.com>
2024-06-13 13:35:33 +0700

    Добавлен стиль
    (cherry picked from commit 3e737ae3fa46a08d5fb89c4ca7bc3c1eae6e66b7)

--- src/Controller/Crud/UserCrudController.php
+++ src/Controller/Crud/UserCrudController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller\Crud;
 
 use App\Entity\Privilege;
@@ -33,6 +35,9 @@ final class UserCrudController extends AbstractCrudController {
             ->setPageTitle(Crud::PAGE_NEW, 'Добавление пользователя')
             ->setPageTitle(Crud::PAGE_EDIT, 'Редактирование пользователя')
             ->setPageTitle(Crud::PAGE_DETAIL, 'Просмотр пользователя')
+            ->overrideTemplates([
+                'layout' => 'admin/user_layout.html.twig',
+            ])
             ;
     }
 
@@ -54,7 +59,7 @@ final class UserCrudController extends AbstractCrudController {
             ->setFormType(PasswordType::class)
             ->setFormTypeOption('empty_data', '')
             ->setRequired(false)
-            ->setHelp('Оставьте поле пустым, если не хотите изменять текущий пароль пароль!')
+            ->setHelp('Оставьте поле пустым, если не хотите изменять текущий пароль!')
             ->onlyWhenUpdating();
 
         yield TextField::new('plainPassword')
