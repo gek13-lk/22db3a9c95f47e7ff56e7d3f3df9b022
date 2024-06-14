5d57518be3fbb331a19823055514e9e6843dc68b
Yuriy Yurinskiy <yuriyyurinskiy@yandex.ru>
2024-06-14 23:37:46 +0700

    правки
    (cherry picked from commit 0bdf8bd90aa3f796947099f884be91deb3b1ec35)

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -11,10 +11,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Security\Permission;
 use Symfony\Component\ExpressionLanguage\Expression;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Logout\LogoutUrlGenerator;
 use function Symfony\Component\Translation\t;
 
 class DashboardController extends AbstractDashboardController
@@ -42,9 +44,23 @@ class DashboardController extends AbstractDashboardController
     }
 
     public function configureUserMenu(UserInterface $user): UserMenu {
-        return parent::configureUserMenu($user)->addMenuItems([
-            MenuItem::linkToRoute('Профиль', null, 'app_profile'),
-        ]);
+        $userMenuItems = [
+            MenuItem::linkToRoute('Профиль', 'icon-user1', 'app_profile'),
+        ];
+
+        if (class_exists(LogoutUrlGenerator::class)) {
+            $userMenuItems[] = MenuItem::section();
+            $userMenuItems[] = MenuItem::linkToLogout(t('user.sign_out', domain: 'EasyAdminBundle'), 'icon-log-out1');
+        }
+
+        $userName = method_exists($user, '__toString') ? (string) $user : $user->getUserIdentifier();
+
+        return UserMenu::new()
+            ->displayUserName()
+            ->displayUserAvatar()
+            ->setName($userName)
+            ->setAvatarUrl(null)
+            ->setMenuItems($userMenuItems);
     }
 
     public function configureMenuItems(): iterable {
