cabdf1e1605ad4bd31c3bb4805b12189721ce74d
gek135 <hyzazavum1>
2024-06-15 10:33:08 +0700

    Проставление времени
    (cherry picked from commit 0531bb4780ed7b05c6a0b1c030ec9e602e56b4d4)

--- src/Controller/ScheduleController.php
+++ src/Controller/ScheduleController.php
@@ -10,53 +10,41 @@ use App\Modules\Algorithm\SetTimeAlgorithmService;
 use App\Modules\Navbar\DefaultNavItem;
 use App\Modules\Navbar\NavElementInterface;
 use App\Modules\Navbar\NavItemInterface;
-use App\Repository\CalendarRepository;
-use App\Repository\DoctorRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Option\EA;
-use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class ScheduleController extends DashboardController {
+class ScheduleController extends AbstractController implements NavElementInterface {
+
+    public function __construct(private EntityManagerInterface $entityManager, private AlgorithmWeekService $service3, private SetTimeAlgorithmService $timeAlgorithmService) {}
     #[Route('/schedule', name: 'app_schedule')]
-    public function schedule(Request $request, CalendarRepository $calendarRepository, DoctorRepository $doctorRepository, AlgorithmWeekService $service3, SetTimeAlgorithmService $timeAlgorithmService): Response {
+    public function index(): Response {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
-        $start = $request->get('dateStart', '2024-01-01');
-        $end = $request->get('dateEnd', '2024-01-31');
+        $countSchedule = 5;
+        $user = $this->getUser();
+        $this->service3->run(new \DateTime('2024-01-01'), new \DateTime('2024-01-09'), $countSchedule);
+        ////$schedule = $this->entityManager->getRepository(TempSchedule::class)->find(2);
 
-        $dateStart = \DateTime::createFromFormat('Y-m-d', $start);
-        $dateEnd = \DateTime::createFromFormat('Y-m-d', $end);
-
-        if ($this->isGranted('ROLE_HR') || $this->isGranted('ROLE_MANAGER')) {
-            $doctors = $doctorRepository->findAll();
-        } else {
-            $doctors = $doctorRepository->findBy(['id' => 1]); // TODO: сделать связку пользователя с врачом
-        }
+        //$this->timeAlgorithmService->setTime($schedule);
+        //$this->service3->run(new \DateTime('2024-01-01'), new \DateTime('2024-01-09'));
 
         return $this->render('schedule/index.html.twig', [
-            'title' => 'Расписание',
-            'calendars' => $calendarRepository->getRange($dateStart, $dateEnd),
-            'doctors' => $doctors,
+            'controller_name' => 'ScheduleController',
+            'username' => $user->getUserIdentifier(),
         ]);
     }
 
-    #[Route('/schedule/run', name: 'app_schedule_run')]
-    public function run(Request $request, AlgorithmWeekService $service3, SetTimeAlgorithmService $timeAlgorithmService): Response {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+    public function getNavItem(): NavItemInterface {
+        return new DefaultNavItem(
+            'Расписание',
+            '<i class="fa-solid fa-calendar-days"></i>',
+            'app_schedule'
+        );
+    }
 
-        $start = $request->get('dateStart', '2024-01-01');
-        $end = $request->get('dateEnd', '2024-01-31');
-
-        $dateStart = \DateTime::createFromFormat('Y-m-d', $start);
-        $dateEnd = \DateTime::createFromFormat('Y-m-d', $end);
-
-        $countSchedule = 1;
-        $service3->run($dateStart, $dateEnd, $countSchedule);
-
-        return $this->json([]);
+    public static function getPriority(): int {
+        return 0;
     }
 }
--- src/Modules/Algorithm/SetTimeAlgorithmService.php
+++ src/Modules/Algorithm/SetTimeAlgorithmService.php
@@ -1,9 +1,9 @@
 <?php
 
-declare(strict_types=1);
 
 namespace App\Modules\Algorithm;
 
+use App\Entity\Doctor;
 use App\Entity\TempDoctorSchedule;
 use App\Entity\TempSchedule;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,10 +15,12 @@ class SetTimeAlgorithmService
     private const START_NIGHT_WORK_HOUR = 20;
     private const START_NIGHT_WORK_MINUTES = 0;
     private const DEFAULT_OFF_TIME = 30;
+    private const MAX_HOUR_WEEK = 36;
+    private const MAX_HOUR_MONTH = 155;
 
     public function __construct(private readonly EntityManagerInterface $em) {}
 
-    public function setTime(TempSchedule $tempSchedule): void
+    public function setTimeByTempSchedule(TempSchedule $tempSchedule): void
     {
         $doctorSchedules = $this->em->getRepository(TempDoctorSchedule::class)->findByTempSchedule($tempSchedule);
 
@@ -64,5 +66,100 @@ class SetTimeAlgorithmService
         }
 
         $this->em->flush();
+    }
+
+    public function setTimeByDoctor(Doctor $doctor, \DateTime $currentDay, array $doctorStat): array
+    {
+        $lastShiftType = null;
+        $doctorWorkSchedule = $doctor->getWorkSchedule();
+        $currentWeek = (clone $currentDay)->modify('Monday this week');
+        //$currentWeekString = (clone $currentWeek)->format('Y-m-d');
+        $startTime = (clone $currentDay)->setTime(self::START_WORK_HOUR, self::START_WORK_MINUTES);
+        $offTime = self::DEFAULT_OFF_TIME;
+        switch ($doctorWorkSchedule->getType()) {
+            case 'Сутки через трое':
+                //$offTime = 60;
+                break;
+            case 'Два выходных':
+                break;
+            case 'Ночные смены':
+                $startTime = (clone $currentDay)->setTime(self::START_NIGHT_WORK_HOUR, self::START_WORK_MINUTES);
+                break;
+            case 'Дневные смены':
+                break;
+            case 'День-ночь':
+                if (isset($doctorStat[$doctor->getId()]['lastShiftType'])) {
+                    if ($doctorStat[$doctor->getId()]['lastShiftType'] == 'День') {
+                        $startTime = (clone $currentDay)->setTime(self::START_NIGHT_WORK_HOUR, self::START_WORK_MINUTES);
+                        $lastShiftType = 'Ночь';
+                    } else {
+                        $lastShiftType = 'День';
+                    }
+                } else {
+                    if (rand(0, 1) == 1) {
+                        $startTime = (clone $currentDay)->setTime(self::START_NIGHT_WORK_HOUR, self::START_WORK_MINUTES);
+                        $lastShiftType = 'Ночь';
+                    } else {
+                        $lastShiftType = 'День';
+                    }
+                }
+
+
+                break;
+        }
+
+        $doctorWorkHoursCount = $doctorWorkSchedule->getHoursPerShift() * $doctor->getStavka();
+
+        if ($doctorWorkHoursCount >= 11) {
+            $offTime = 60;
+        }
+
+        $doctorWorkMinutes = $doctorWorkHoursCount * 60;
+        $endTime = (clone $startTime)->modify(' +'. (self::START_WORK_MINUTES + $offTime + $doctorWorkMinutes). ' minutes');
+        $workHours = (clone $endTime)->modify('- '.$offTime.' minutes')->diff($startTime);
+
+        if ($workHours->days) {
+            $workHours = 24 + $workHours->h;
+        } else {
+            $workHours = $workHours->h;
+        }
+
+        $weekHours = 0;
+        $weeksCount = 0;
+        $monthHours = 0;
+
+        foreach ($doctorStat[$doctor->getId()]['days'] ?? [] as $date => $week) {
+            //TODO: Обнуление времени, если прошел месяц
+            //if ($weeksCount )
+            $weekHours = 0;
+            if (isset($week[$doctor->getId()])) {
+                $weekHours = array_sum($week[$doctor->getId()]);
+            }
+
+            $weekString = (new \DateTime($date));
+            if ($currentWeek->diff($weekString)->m == 0) {
+                $monthHours = $monthHours + $weekHours;
+            } else {
+                $monthHours = 0;
+            }
+
+            $weeksCount++;
+        }
+
+        $ostHoursWeek = self::MAX_HOUR_WEEK - $weekHours;
+
+        if ($ostHoursWeek < $workHours) {
+            $diff = $workHours - $ostHoursWeek;
+            $endTime->modify('- '.$diff.' hours');
+            $workHours = $ostHoursWeek;
+        }
+
+        return [
+            'hours' => $workHours,
+            'start' => $startTime,
+            'end' => $endTime,
+            'off' => $offTime,
+            'lastShiftType' => $lastShiftType,
+        ];
     }
 }
